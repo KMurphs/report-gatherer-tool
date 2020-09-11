8731ca6770560e6bf657f68fa44d28ad95acd22b
stephane kibonge <stephane.kibonge@etion.co.za>
2020-09-11 10:01:42 +0200

    Add freezing functionality with class modification and javascript elimination

--- server.py
+++ server.py
@@ -9,16 +9,20 @@ import websockets
 
 async def serve_requests(websocket, path):
     print("Serving Client at: ", path)
+    
     if path == "/":
         while True:
             now = datetime.datetime.utcnow().isoformat() + "Z"
             await websocket.send(now)
             await asyncio.sleep(random.random() * 3)
+
     elif path == "/save":
         msg = await websocket.recv()
+        msg = msg.replace('disable-on-frozen','disable-on-frozen--active')
+        msg = msg.replace('<script src="client.js"></script>', '<!-- <script src="client.js"></script> -->')
         with open("frozen_client.html", "w") as f:
             f.writelines(msg)
-        print(msg)
+
     else:
         print("Unsupported Path")
 
