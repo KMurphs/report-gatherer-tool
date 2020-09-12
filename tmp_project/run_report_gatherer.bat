fd2741b7ca2eaec3d1cd08007d0dd8d8dfc11ff5
stephane kibonge <stephane.kibonge@etion.co.za>
2020-09-12 22:24:28 +0200

    Add client.html creation code in run_report_gatherer

--- tmp_project/run_report_gatherer.bat
+++ tmp_project/run_report_gatherer.bat
@@ -194,6 +194,21 @@ ECHO.
 ECHO.
 ECHO.
 
+REM CREATE html client file
+(
+	ECHO.^<!DOCTYPE html^>
+	ECHO.^<html^>
+	ECHO.    ^<head^>
+	ECHO.        ^<title^>[%order%] Serial Number Status^</title^>
+	ECHO.        ^<link rel="stylesheet" href="html_utils/styles/style.css"^>
+	ECHO.    ^</head^>
+	ECHO.    ^<body^>
+ 	ECHO.        ^<div id="app-root"^>^</div^>
+ 	ECHO.        ^<script src="html_utils/scripts/client.js"^>^</script^>
+ 	ECHO.    ^</body^>
+ 	ECHO.^</html^>
+)>..\common\client\client2.html
+
 
 REM Start HTML Client for websocket server
 SET html_client=%~dp0
--- tmp_project/test.bat
+++ tmp_project/test.bat
@@ -1,2 +1,13 @@
-@echo off
-for /f "tokens=*" %G in ('dir /b /s /a:d "*"') do echo Found %G
+(
+	ECHO.^<!DOCTYPE html^>
+	ECHO.^<html^>
+	ECHO.    ^<head^>
+	ECHO.        ^<title^>[%order%] Serial Number Status^</title^>
+	ECHO.        ^<link rel="stylesheet" href="html_utils/styles/style.css"^>
+	ECHO.    ^</head^>
+	ECHO.    ^<body^>
+ 	ECHO.        ^<div id="app-root"^>^</div^>
+ 	ECHO.        ^<script src="html_utils/scripts/client.js"^>^</script^>
+ 	ECHO.    ^</body^>
+ 	ECHO.^</html^>
+)>..\common\client\client2.html
