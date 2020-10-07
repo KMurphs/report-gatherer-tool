6fa520bfb51d37643046ecc425fb6344512e9f44
stephane kibonge <stephane.kibonge@etion.co.za>
2020-10-07 20:50:16 +0200

    Updated Config in backend

--- event.handlers/config.handler.js
+++ event.handlers/config.handler.js
@@ -11,29 +11,64 @@ const config = {
 
   handle: async function(appContext, data){
 
+
+    const configFilePath = path.join(__dirname, config.file);
+
+
     // if data is null read config file
     if(!config.data){
 
-      const configFilePath = path.join(__dirname, config.file);
-
       await fs.promises.readFile(configFilePath, { encoding: 'utf-8' })
       .catch(err => { console.error(`Could not read file '${config.file}': `, err); config.writeToFile(); })
-      .then(data => config.data = JSON.parse(data))
+      .then(data => config.data = data ? JSON.parse(data) : config.defaults)
       .catch(async err => { 
         console.error(`Could not convert content of file '${config.file}' to json: `, err); 
         await fs.promises.copyFile(configFilePath, configFilePath.replace(/json$/, 'backup.json'))
         config.writeToFile(); 
+        config.data = config.defaults;
       })
 
     }
+    console.log(config.data)
 
 
-    // If nothing in data, send our config data
-    if(!data) { return appContext.reply(config.data); }
+    // If string in data, send our config data for the project in string
+    if(typeof(data) === "string") {
+      const project = data;
+      if(!config.data[project]){ 
+        config.data[project] = config.defaults;
+        config.data[project]["project_name"] = project;
+      }
+      await fs.promises.writeFile(configFilePath, JSON.stringify(config.data))
+      return appContext.reply(config.data[project]); 
+    }
     
 
+
+
     // else merge with our copy of config data
-    appContext.reply(config.data);
+    const project = data["project_name"];
+    if(!project) appContext.reply("'project_name' field is missing");
+
+    for(let key in data){
+
+      console.log(key);
+
+      switch(key){
+        case "directories_to_look_for_reports":
+        case "tests_to_validate_reports":
+          config.data[project][key] = [...data[key]];
+          break;
+
+        default:
+          config.data[project][key] = data[key];
+          break;
+      }
+
+    }
+    await fs.promises.writeFile(configFilePath, JSON.stringify(config.data))
+    return appContext.reply(config.data[project]); 
+
   },
 
 
@@ -42,7 +77,7 @@ const config = {
 
 
 
-  writeToFile: function(data = null){
+  writeToFile: function( data = null ){
     let defaultObj = {}
     !data && (defaultObj[config.defaults.project_name] = config.defaults)
 
